571740c558f42ffb747bee5eb6e88054026dc37b
Petra696 <91752761+Petra696@users.noreply.github.com>
2021-11-20 11:59:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log('Hello Winc Academy');
+var myNm = { firstname: "Petra" };
+console.log(myNm);
+
+let m = 4;
+let n = 4;
+let o = m + n;
+document.getElementById("Addition1").innerHTML = o;
+console.log(Addition1);
+
+let p = "4";
+let q = "4";
+let r = p + q;
+document.getElementById("Addition2").innerHTML = r;
+console.log(Addition2);
+
+let x = 5;
+let y = 2;
+let z = x + y;
+document.getElementById("Addition").innerHTML = z;
+console.log(Addition);
+
+let a = 5;
+let b = 2;
+let c = a - b;
+document.getElementById("Subtraction").innerHTML = c;
+console.log(Subtraction);
+
+let d = 5;
+let e = 2;
+let f = d * e;
+document.getElementById("Multiplication").innerHTML = f;
+console.log(Multiplication);
+
+let g = 5;
+let h = 2;
+let i = g / h;
+document.getElementById("Division").innerHTML = i;
+console.log(Division);
+
+let j = 5;
+let k = 2;
+let l = j % k;
+document.getElementById("Modulus").innerHTML = l;
+console.log(Modulus);
+
+console.log(typeof 45);
+//expected output: "number"
+
+console.log(typeof fourtyfive);
+//exptected output: "text"
